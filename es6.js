8b66b9f5d7a5bbb33bd443bffce3126acdfc4fe4
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-05 07:58:59 -1000

    setting default parameters

--- es6.js
+++ es6.js
@@ -63,3 +63,6 @@ const myConcat = (arr1, arr2) => {
     return arr1.concat(arr2);
 };
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+// Setting default parameters to a function
+const increment = (number, value = 1) => number + value;
