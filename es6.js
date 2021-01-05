8ad7314cafcb56973277cc15fa146148156a0d23
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-05 07:44:31 -1000

    Preventing changes to a const using freeze

--- es6.js
+++ es6.js
@@ -36,3 +36,18 @@ function editInPlace() {
     s[2] = 7;
 }
 editInPlace();
+
+// Preventing object mutation
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    Object.freeze(MATH_CONSTANTS);
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
