120d8f157e45969ee674a1a08d00f18923e71569
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-05 07:31:13 -1000

    comparing the scope of var and let keywords

--- es6.js
+++ es6.js
@@ -7,3 +7,14 @@ function catTalk() {
     quote = catName + " says Meow!";
 }
 catTalk();
+
+// Comparing the scope of var and let keywords
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+        let i = 'block scope';
+        console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+}
