a2fba2894c73ba2e534993ddd4c985c75b9f857f
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-29 13:46:49 -1000

    destructuring continues

--- es6.js
+++ es6.js
@@ -125,3 +125,15 @@ function removeFirstTwo(list) {
   return arr;
 }
 const arr = removeFirstTwo(source);
+
+//Destructing to pass an object as a functions parameters
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = ({max, min }) => (max + min) / 2.0; 
