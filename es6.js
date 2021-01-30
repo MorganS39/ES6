63580ebc6fb98cd25535a5ec80bb2fb4dbbd71f0
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-29 15:08:44 -1000

    JS promise training (i promise)

--- es6.js
+++ es6.js
@@ -239,3 +239,54 @@ import subtract from "./math_functions.js";
 
 subtract(7,4);
 
+// Create a JS promise
+const makeServerRequest = new Promise((resolve, reject) => {
+
+});
+
+//Complete a promise with resolve and reject
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+//Handle a fulfilled promise with 'then'
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+  makeServerRequest.then(result => {
+    console.log(result);
+  });
+});
+
+//Handle a rejected promise with catch
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+
+makeServerRequest.catch(error => {
+  console.log(error);
+});
